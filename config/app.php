b001165d78fdb2d25d0e001424d7b67e43c44204
hrzqrz <hrzqrz@gmail.com>
2020-06-27 09:02:45 +0430

    Installing laravel file manager & laravel CK editor

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
 
     ],
 
